f62c46e2ce7c46cd4a84e9c5f3bf04128d87c63f
rafal_gcp <rufi156@gmail.com>
2026-02-08 23:38:14 +0000

    change to sequential clients - parallel clients caused errors
    something about missing *_current_task_reconstucted_data.pth file and num_samples=0 in predict_from_gen
    
    I suspect concurrent access to yet not existing files or some other I/O concurrency issue.

--- main_fcl.py
+++ main_fcl.py
@@ -189,7 +189,7 @@ def run(args):
         if gpu_flag == 1:
             client_res = {"num_gpus": num_GPUs / n_cl, "num_cpus": num_CPUs}
         else:
-            client_res = {"num_gpus": num_GPUs, "num_cpus": num_CPUs / n_cl}
+            client_res = {"num_gpus": num_GPUs, "num_cpus": num_CPUs}
 
         if not os.path.exists(f"{args.output}/{n_cl}"):
             os.mkdir(f"{args.output}/{n_cl}")
