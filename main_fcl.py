5ae59fda5321f3e9ac3b8097f88c5c7790b1896b
rufi156 <33355829+rufi156@users.noreply.github.com>
2026-02-08 22:43:19 +0100

    remove loading unknown pretrained weights
    There is no information about where do the pretrained weights come from neither in the repo nor in the paper.

--- main_fcl.py
+++ main_fcl.py
@@ -433,24 +433,24 @@ def run(args):
                             os.mkdir(f"{output_root_LGR}")
                         if args.model == 'MobileNet':
                             input_dim = 1280  # Replace with the size of your input data
-                            if gpu_flag == 1:
-                                with open('models/gpu/MobileNet.pkl', 'rb') as f:
-                                    net.load_state_dict(pickle.load(f), strict=True)
-                                print("-------------------Loaded pretrained MobileNet for GPU-------------------")
-                            else:
-                                with open('models/cpu/MobileNet.pkl', 'rb') as f:
-                                    net.load_state_dict(pickle.load(f), strict=True)
-                                print("-------------------Loaded pretrained MobileNet for CPU-------------------")
+                            # if gpu_flag == 1:
+                            #     with open('models/gpu/MobileNet.pkl', 'rb') as f:
+                            #         net.load_state_dict(pickle.load(f), strict=True)
+                            #     print("-------------------Loaded pretrained MobileNet for GPU-------------------")
+                            # else:
+                            #     with open('models/cpu/MobileNet.pkl', 'rb') as f:
+                            #         net.load_state_dict(pickle.load(f), strict=True)
+                            #     print("-------------------Loaded pretrained MobileNet for CPU-------------------")
                         elif args.model == 'DeepLabMobileNet':
                             input_dim = 1344
-                            if gpu_flag == 1:
-                                with open('models/gpu/DeepLabMobileNet.pkl', 'rb') as f:
-                                    net.load_state_dict(pickle.load(f), strict=True)
-                                print("-------------------Loaded pretrained DeepLabMobileNet for GPU-------------------")
-                            else:
-                                with open('models/cpu/DeepLabMobileNet.pkl', 'rb') as f:
-                                    net.load_state_dict(pickle.load(f), strict=True)
-                                print("-------------------Loaded pretrained DeepLabMobileNet for CPU-------------------")
+                            # if gpu_flag == 1:
+                            #     with open('models/gpu/DeepLabMobileNet.pkl', 'rb') as f:
+                            #         net.load_state_dict(pickle.load(f), strict=True)
+                            #     print("-------------------Loaded pretrained DeepLabMobileNet for GPU-------------------")
+                            # else:
+                            #     with open('models/cpu/DeepLabMobileNet.pkl', 'rb') as f:
+                            #         net.load_state_dict(pickle.load(f), strict=True)
+                            #     print("-------------------Loaded pretrained DeepLabMobileNet for CPU-------------------")
                         # params = get_parameters(net.conv_module)
                         latent_dim = 64  # Set according to your desired latent space dimension
                         encoder_units = [256, 128]  # Adjust as needed
