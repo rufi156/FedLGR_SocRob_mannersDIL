rufi156/FedLGR_SocRob_mannersDIL

commit 16610c19d35082bb015e8516b3751d34b4d7098f

remove evaluation to speed up training
fraction_evaluate = 0.0

can also be passed when instantiating in main_fcl.py

server/strategies.py
@@ -17,6 +17,10 @@ def weighted_avg(results: List[Tuple[int, float, Optional[float]]]) -> float:
 
 
 class FedAvgWithAccuracyMetric(fl.server.strategy.FedAvg):
+	def __init__(self, *args, **kwargs):
+		kwargs.setdefault("fraction_evaluate", 0.0)
+		super().__init__(*args, **kwargs)
+		
 	def aggregate_evaluate(self,
 						   rnd: int,
 						   results: List[Tuple[ClientProxy, EvaluateRes]],

commit 52b99e442ffb4a3bcda067de9da37b20276e73b9

reverse commit 16610c19d35082bb015e8516b3751d34b4d7098f
caused errors on parsing history from training, refactoring to do it properly not worth it for now

server/strategies.py
@@ -17,10 +17,6 @@ def weighted_avg(results: List[Tuple[int, float, Optional[float]]]) -> float:
 
 
 class FedAvgWithAccuracyMetric(fl.server.strategy.FedAvg):
-	def __init__(self, *args, **kwargs):
-		kwargs.setdefault("fraction_evaluate", 0.0)
-		super().__init__(*args, **kwargs)
-		
 	def aggregate_evaluate(self,
 						   rnd: int,
 						   results: List[Tuple[ClientProxy, EvaluateRes]],